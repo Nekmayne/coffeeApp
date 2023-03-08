541d6083ab8c28be34c3b0cfecdffd6ec3503bcd
Nekmayne <bgm035@myy.haaga-helia.fi>
2023-03-08 15:17:55 +0200

    Cleaned up code

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
-const app = require("./app");
-const http = require("http");
-const server = http.createServer(app);
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const coffeeController = require("./controllers/coffees");
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use("/api/coffees", coffeeController);
 
 const PORT = 3001;
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
